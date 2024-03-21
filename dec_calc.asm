d49edec85a903ce9d86a6547523c7f3f78b8e9ed
Artem Miroshnikov <103366005+kocery@users.noreply.github.com>
2024-03-21 12:15:07 +0700

    dec_calc

---
+++ dec_calc.asm
@@ -0,0 +1,21 @@
+.include "libs/dec_lib.asm"
+
+main:
+	call read_dec
+  mv s9, a0
+  println
+  
+  call read_dec
+  mv s10, a0
+  println
+  
+  mv a0, s9
+  mv a1, s10
+  call readop
+	
+	call print_dec
+	
+	j end
+
+end:
+	exit 0
